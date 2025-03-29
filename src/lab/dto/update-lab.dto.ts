cb655ee0390d23c12b56df77695afedcd395badf
PickAUsername <67839920+Skul1CrowN@users.noreply.github.com>
2025-03-29 17:06:02 +0700

    Dev (#53)
    * fix: flow of image-lab service
    
    * fix(lab): default isReady to false

--- src/lab/dto/update-lab.dto.ts
+++ src/lab/dto/update-lab.dto.ts
@@ -1,6 +1,12 @@
-import { OmitType, PartialType } from "@nestjs/swagger";
+import { ApiProperty, OmitType, PartialType } from "@nestjs/swagger";
 import { CreateLabDto } from "./create-lab.dto";
+import { IsBoolean, IsOptional } from "class-validator";
 
 export class UpdateLabDto extends PartialType(
   OmitType(CreateLabDto, ["labImageId"] as const),
-) {}
+) {
+  @ApiProperty({ type: Boolean })
+  @IsOptional()
+  @IsBoolean()
+  isReady: boolean;
+}
